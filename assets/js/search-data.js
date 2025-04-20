80dda8a94b508c25799ccf5de3a236ae150cb03a
prakritsedhai <32109503+prakritsedhai@users.noreply.github.com>
2025-04-20 20:44:44 +0000

    Deploying to gh-pages from @ prakritsedhai/prakritsedhai.github.io@fcb89a444b1280a649305215edfe99af62ab0932 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "Why did the developer go broke? Because he kept pushing to **master** but never made a **commit** to his savings account.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
